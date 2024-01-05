eecc9b8139e3698fde733baeaf9393819f72f1bb
Karina <Karina@10.0.0.31>
2024-01-05 00:15:29 -0500

    Practicing line

--- src/f/DemoF.java
+++ src/f/DemoF.java
@@ -5,7 +5,11 @@ public class DemoF {
 	public static void main (String args[]) {
 		
 		System.out.println("Saturday...");
+	
+		//coming from local repo
+		System.out.println("Practicing git push");
 		
 	}
+
 
 }
